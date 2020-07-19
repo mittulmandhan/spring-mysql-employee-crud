92c748a876cb724d789a9841fab82bb15b92cad5
Mittul Mandhan <conanmittul@gmail.com>
2020-07-19 05:30:48 +0530

    use log4j2(easier way) for logging

--- src/main/java/com/employee/controller/LoggingController.java
+++ src/main/java/com/employee/controller/LoggingController.java
@@ -1,21 +1,21 @@
 package com.employee.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
+@Log4j2
 public class LoggingController {
-	Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
 	@RequestMapping("/")
 	public String index() {
-		logger.trace("A TRACE Message");
-		logger.debug("A DEBUG Message");
-		logger.info("An INFO Message");
-		logger.warn("A WARN Message");
-		logger.error("An ERROR Message");
+		log.trace("A TRACE Message");
+		log.debug("A DEBUG Message");
+		log.info("An INFO Message");
+		log.warn("A WARN Message");
+		log.error("An ERROR Message");
 
 		return "checkout the logs to see the output...";
 	}
